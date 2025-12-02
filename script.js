95d842d6004b2dca05705cd8c88c58952ee15ce0
Denis_levenko <denislevenko0107@gmail.com>
2025-12-03 00:20:34 +0200

    1.3 code.js

--- script.js
+++ script.js
@@ -15,4 +15,10 @@ const h1 = document.querySelector("h1");
 const h1FontSize = h1 ? getComputedStyle(h1).fontSize : "h1 немає на сторінці";
 console.log("font-size тега <h1>:", h1FontSize);
 
+button.addEventListener("click", () => {
+    const isRed = document.body.style.background === "red";
+
+    document.body.style.background = isRed ? "white" : "red";
+    document.body.style.color = isRed ? "black" : "white";
+});
 
