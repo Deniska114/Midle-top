b44d822b196c46a18c06bd7bad2b8eee329ccc49
Denis_levenko <denislevenko0107@gmail.com>
2025-12-03 00:19:13 +0200

    1.2 code.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 
-const button = document.getElementById("changeBg");
-
-button.addEventListener("click", () => {
-  if (document.body.style.background === "red") {
-    document.body.style.background = "white";
-    document.body.style.color = "black";
-  } else {
-    document.body.style.background = "red";
-    document.body.style.color = "white";
-  }
-});
-
-
-
-
 const pCount = document.querySelectorAll("p").length;
 console.log("Кількість параграфів <p>:", pCount);
 
@@ -23,132 +8,11 @@ console.log("Кількість заголовків <h2>:", h2Count);
 
 
 const bodyBg = getComputedStyle(document.body).backgroundColor;
-console.log("background-color <body>:", bodyBg);
+console.log("background-color тега <body>:", bodyBg);
 
 
 const h1 = document.querySelector("h1");
-if (h1) {
-  const h1Font = getComputedStyle(h1).fontSize;
-  console.log("font-size <h1>:", h1Font);
-} else {
-  console.log("На сторінці немає <h1>");
-}
+const h1FontSize = h1 ? getComputedStyle(h1).fontSize : "h1 немає на сторінці";
+console.log("font-size тега <h1>:", h1FontSize);
 
 
-setTimeout(() => {
-    addDynamicImages();
-}, 5000);
-
-function addDynamicImages() {
-
-    
-    let imagesUrl = [
-        "https://via.placeholder.com/200x200/ff0000",
-        "https://via.placeholder.com/200x200/00ff00",
-        "https://via.placeholder.com/200x200/0000ff",
-        "https://via.placeholder.com/200x200/f0f000"
-    ];
-
-    
-    const parent = document.querySelector(".products");
-
-   
-    const fragment = document.createDocumentFragment();
-
-    
-    imagesUrl.forEach((url, index) => {
-        setTimeout(() => {
-            
-            const li = document.createElement("li");
-
-           
-            const img = document.createElement("img");
-            img.src = url;
-            img.alt = "Dynamic Image";
-            img.style.width = "200px";
-
-            
-            li.appendChild(img);
-
-            
-            fragment.appendChild(li);
-
-            
-            parent.appendChild(fragment);
-
-        }, index * 1000);
-    });
-}
-
-document.querySelector("form").addEventListener("submit", function (event) {
-    event.preventDefault(); // не відправляємо форму
-
-    let emailLogin = document.getElementById("email").value;
-
-  
-    const loginRegex = /^[a-zA-Z0-9_]{3,16}$/;   
-    const emailRegex = /^[\w.-]+@[\w.-]+\.\w{2,}$/;
-    const phoneRegex = /^\+380\d{9}$/;
-
-    console.log("Перевіряємо:", emailLogin);
-
-    
-    if (emailLogin.match(loginRegex)) {
-        alert("Ви ввели ЛОГІН ");
-        return;
-    }
-
-   
-    if (emailLogin.search(emailRegex) !== -1) {
-        alert("Ви ввели EMAIL ");
-        return;
-    }
-
-    
-    if (emailLogin.match(phoneRegex)) {
-        alert("Ви ввели НОМЕР ТЕЛЕФОНУ ");
-        return;
-    }
-
-    alert(" Невірний формат. Введіть login / email / телефон.");
-});
-
-
-
-document.getElementById("email").addEventListener("input", function () {
-    let text = this.value;
-
-    
-    let cleaned = text.replace(/[^\w]/g, "");
-
-    if (cleaned !== text) {
-        this.value = cleaned;
-        console.log("Очищено login:", cleaned);
-    }
-});
-
-document.getElementById("checkBtn").addEventListener("click", () => {
-    let text1 = document.getElementById("input1").value.toLowerCase();
-    let text2 = document.getElementById("input2").value.toLowerCase();
-
-    
-    text1 = text1.replace(/[.,!?]/g, "");
-    text2 = text2.replace(/[.,!?]/g, "");
-
-  
-    let arr1 = text1.split(" ");
-    let arr2 = text2.split(" ");
-
-  
-    let set1 = new Set(arr1);
-    let set2 = new Set(arr2);
-
-    
-    let common = [...set1].filter(word => set2.has(word));
-
-   
-    document.getElementById("result").textContent = 
-        common.length > 0 
-        ? "Спільні слова: " + common.join(", ") 
-        : "Спільних слів немає";
-});
